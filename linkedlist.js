86eb3edbe2d4e6460d653fac82e3c7de0a3ce117
shohan494 <shohan12224943@gmail.com>
2017-11-16 21:33:30 +0600

    origibal source code added

---
+++ linkedlist.js
@@ -0,0 +1,34 @@
+function LinkedList() {
+
+    let Node = function(element){
+
+        this.element = element;
+        this.next = null;
+    };
+
+    let length = 0;
+    let head = null;
+
+    this.append = function(element){
+
+        let node = new Node(element),
+            current;
+
+        if (head === null){ //first node on list
+            head = node;
+        } else {
+
+            current = head;
+
+            //loop the list until find last item
+            while(current.next){
+                current = current.next;
+            }
+
+            //get last item and assign next to added item to make the link
+            current.next = node;
+        }
+
+        length++; //update size of list
+    };
+}
